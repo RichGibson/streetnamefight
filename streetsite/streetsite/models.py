c0834645a71a30653df14e071a4d92f5d0f113d6
Rich Gibson <rich.gibson@gmail.com>
2014-02-12 23:26:05 +0100

    added some spam checking, a robots.txt, and a tiny bit of cleanup

--- streetsite/streetsite/models.py
+++ streetsite/streetsite/models.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 
 class Fight(models.Model):
     word = models.TextField()
+    ip = models.TextField()
+
+class BadFight(models.Model):
+    word = models.TextField()
+    ip = models.TextField()
